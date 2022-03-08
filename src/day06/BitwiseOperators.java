de05a5dce1830ef8f141b2449583256f5d064668
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-08 19:07:49 +0300

    Huseyin Kartal düzenledi

--- src/day06/BitwiseOperators.java
+++ src/day06/BitwiseOperators.java
@@ -3,6 +3,19 @@ package day06;
 public class BitwiseOperators {
     public static void main(String[] args) {
 
+        //Bitwise Operators
+
+        //&, |, ^,~,<<,>>,>>>
+
+        int x=5;  // 00000101 (32 bitten sadece 8 ini yazdık)
+        int y=7; //  00000111
+
+        int r=x&y;
+
+        System.out.println(r);
+
+
+
 
     }
 }
